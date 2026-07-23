f3e5fdf1fadba8ab7301eea9398d356ee5244ef2
anuja-naik <naikanuja80@gmail.com>
2026-07-23 21:37:28 +0530

    Time: 1 ms (98.16%) | Memory: 47.7 MB (43.38%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,41 +1,36 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length; // rows
-        int n = matrix[0].length; // columns
+        int m = matrix.length;
+        int n = matrix[0].length;
         int col0 = 1;
-
-        // chk for zero and mark row 0 and col 0
         for(int i=0; i<m; i++){
             if(matrix[i][0] == 0){
                 col0 = 0;
             }
-            for(int j=1; j<n; j++){ // Starts from 1 as col0 is used
-                if(matrix [i][j] == 0){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
-        
-        // Traverse remaining arr
+
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                if(matrix [i][j] != 0){
-                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
                         matrix[i][j] = 0;
                     }
                 }
             }
         }
 
-        // Handle first row
         if(matrix[0][0] == 0){
             for(int j=0; j<n; j++){
                 matrix[0][j] = 0;
             }
         }
 
-        // Handle first col
         if(col0 == 0){
             for(int i=0; i<m; i++){
                 matrix[i][0] = 0;
