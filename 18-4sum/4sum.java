cd9000425deae2d48ba9d4aae637ca855d0415f7
anuja-naik <naikanuja80@gmail.com>
2026-09-03 21:09:20 +0530

    Time: 19 ms (82.93%) | Memory: 46.3 MB (14.36%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,46 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
+        Arrays.sort(nums);
         for(int i=0; i<n; i++){
-            if(i>0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            for(int j=i+1; j<n; j++){
-                if(j>i+1 && nums[j]==nums[j-1]){
-                    continue;
-                }
-
-                int left = j+1;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j<n; j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                int left = j + 1;
                 int right = n-1;
-
                 while(left < right){
-                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right]; // To avoid Overflow
-
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
                     if(sum < target){
-                        left ++;
-                    }
+                        left++;
+                    } 
                     else if(sum > target){
                         right --;
                     }
-
-                    else{
-                        list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                    else if(sum == target){
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         left ++;
                         right --;
-
-                        while(left < right && nums[left] == nums[left-1]){
+                        while(left < right && nums[left] == nums[left -1]){
                             left ++;
                         }
-
-                        while(left < right && nums[right] == nums[right+1]){
+                        while(left < right && nums[right] == nums[right +1]){
                             right --;
                         }
                     }
                 }
             }
         }
-        return list;
+        return ans;
     }
 }
