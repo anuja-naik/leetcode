083f14b036ab74a12fc825703e779189b45fec62
anuja-naik <naikanuja80@gmail.com>
2026-09-24 19:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (32.80%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,19 +1,18 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        // Find pivot
         int pivot = -1;
-        for(int i = n-2; i>=0; i--){
+        int n = nums.length;
+
+        for(int i=n-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
-                pivot = i;              // idx 
+                pivot = i;
                 break;
             }
         }
-        // If arr does not have lexicographical larger rearrangement then reverse arr
         if(pivot == -1){
             int i = 0;
             int j = n-1;
-            while(i<j){
+            while(i < j){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
@@ -23,8 +22,7 @@ class Solution {
             return;
         }
 
-        // Find next larger element right after pivot and swap it with pivot
-        for(int i = n-1; i>pivot; i--){
+        for(int i=n-1; i> pivot; i--){
             if(nums[i] > nums[pivot]){
                 int temp = nums[i];
                 nums[i] = nums[pivot];
@@ -33,7 +31,6 @@ class Solution {
             }
         }
 
-        // Reverse elements from pivot+1 to n-1
         int i = pivot + 1;
         int j = n - 1;
         while(i < j){
